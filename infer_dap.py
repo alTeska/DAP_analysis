544403d52fb9adb567824a488569619a7f8a92a2
Aleksandra Teska <aleksandra.teska@gmail.com>
2018-12-26 00:12:42 +0100

    working version

--- infer_dap.py
+++ infer_dap.py
@@ -7,7 +7,7 @@ from delfi.distribution import Uniform, Gaussian
 from delfi.generator import Default
 from delfi.inference import SNPE
 
-from DAPmodel.utils import prior, obs_params, syn_obs_stats
+from DAPmodel.utils import prior, obs_params, syn_obs_stats, syn_obs_data
 from DAPmodel.DAPSumStats import DAPSummaryStatsA
 from DAPmodel.DAPsimulator import DAPSimulator
 from DAPmodel.cell_fitting.read_heka import (get_sweep_index_for_amp,
@@ -17,7 +17,7 @@ from DAPmodel.cell_fitting.read_heka import get_v_and_t_from_heka, shift_v_rest
 # General Settings Pick
 n_rounds = 1
 n_summary = 2
-n_samples = 100
+n_samples = 10
 
 
 # Setup Priors
@@ -38,6 +38,7 @@ sweep_idx = get_sweep_index_for_amp(ramp_amp, protocol)
 v, t = get_v_and_t_from_heka(data_dir, protocol, sweep_idxs=[sweep_idx])
 v = shift_v_rest(v[0], v_shift)
 t = t[0]
+dt = t[1] - t[0]
 i_inj, t_on, t_off = get_i_inj_from_function(protocol, [sweep_idx], t[-1], t[1]-t[0],
                                              return_discontinuities=False)
 
@@ -60,19 +61,50 @@ G = Default(model=M, prior=prior_unif, summary=s)  # Generator
 
 # Runing the simulation
 inf_snpe = SNPE(generator=G, n_components=1, n_hiddens=[2], obs=S,
-                pilot_samples=10)
+                pilot_samples=10, prior_norm=True)
 
-logs, tds, posteriors = inf_snpe.run(n_train=[n_samples], n_rounds=n_rounds)
+logs, tds, posteriors = inf_snpe.run(n_train=[n_samples], n_rounds=n_rounds, proposal=prior_unif)
 
 # Analyse results
-samples_prior = prior_unif.gen(n_samples=10000)
-samples_posterior = posteriors[-1].gen(n_samples=10000)
+samples_prior = prior_unif.gen(n_samples=int(5e5))
+samples_posterior = posteriors[-1].gen(n_samples=int(5e5))
 
 # Plots
-simulation, axes = plt.subplots(2, 2, figsize=(16, 14))
+x_post = syn_obs_data(i_inj[0], dt, posteriors[-1].mean)
+idx = np.arange(0, len(x_o['data']))
+
+simulation, axes = plt.subplots(2, 1, figsize=(16,14))
+axes[0].plot(idx, x_o['I'], c='g', label='goal')
+axes[0].plot(idx, x_post['I'], label='posterior')
+axes[0].set_title('current')
+axes[0].legend()
+
+axes[1].step(idx, x_o['data'], c='g', label='goal')
+axes[1].step(idx, x_post['data'], label='posterior')
+axes[1].set_title('Voltage trace')
+axes[1].legend()
+
+
+distr, axes = plt.subplots(2, 2, figsize=(16, 14))
 
 axes[0, 0].hist(samples_prior[:, 0], bins='auto')
 axes[1, 0].hist(samples_prior[:, 1], bins='auto')
 axes[0, 1].hist(samples_posterior[:, 0], bins='auto')
 axes[1, 1].hist(samples_posterior[:, 1], bins='auto')
+axes[0, 0].set_title('prior')
+axes[1, 0].set_title('prior')
+axes[0, 1].set_title('posterior')
+axes[1, 1].set_title('posterior')
+
+distr, axes = plt.subplots(2, 1, figsize=(16, 14))
+axes[0].hist(samples_prior[:, 0], bins='auto', label='prior')
+axes[1].hist(samples_prior[:, 1], bins='auto', label='prior')
+axes[0].hist(samples_posterior[:, 0], bins='auto', label='posterior')
+axes[1].hist(samples_posterior[:, 1], bins='auto', label='posterior')
+axes[0].legend()
+axes[1].legend()
+
+axes[0].set_title('both')
+axes[1].set_title('both')
+
 plt.show()
